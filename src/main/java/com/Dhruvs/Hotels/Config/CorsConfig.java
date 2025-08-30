13823710aeddd88a89548bb82383907262cf4c06
DHRUV DAVE <dhruvdave.developer@gmail.com>
2025-08-30 14:28:35 +0530

    Update CorsConfig.java

--- src/main/java/com/Dhruvs/Hotels/Config/CorsConfig.java
+++ src/main/java/com/Dhruvs/Hotels/Config/CorsConfig.java
@@ -1,5 +1,13 @@
+package com.Dhruvs.Hotels.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
